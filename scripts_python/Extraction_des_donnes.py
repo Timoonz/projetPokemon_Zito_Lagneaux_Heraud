4fe00ac0e1451df4598c3c45df6d602adf006cd1
abigaelle-lagneaux <abigaelle.lagneaux@gmail.com>
2024-05-06 12:36:11 +0200

    Différenciation des différents types de Pokemons

--- scripts_python/Extraction_des_donnes.py
+++ scripts_python/Extraction_des_donnes.py
@@ -18,8 +18,134 @@ for i in range (998) :
         
         if pokemons_map.iloc[i,0] == types_pokemon.iloc[j,1] :
             
-            Pokemon_map.append(p.Pokemon(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
-                                         types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
-                                         types_pokemon.iloc[j,9], types_pokemon.iloc[j,10], 
-                                         types_pokemon.iloc[j,12],  pokemons_map.iloc[i,1]))
+            if types_pokemon.iloc[j,2] == "Fire" :
             
+                Pokemon_map.append(p.Fire(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                             types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                             types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                             types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                             pokemons_map.iloc[i,1]))
+            
+            if types_pokemon.iloc[j,2] == "Water" :
+                
+                Pokemon_map.append(p.Water(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                           types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                           types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                           types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                           pokemons_map.iloc[i,1]))
+            
+            if types_pokemon.iloc[j,2] == "Bug" :
+                
+                Pokemon_map.append(p.Bug(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                         types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                         types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                         types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                         pokemons_map.iloc[i,1]))
+                
+            if types_pokemon.iloc[j,2] == "Normal" :
+                
+                Pokemon_map.append(p.Normal(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                            types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                            types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                            types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                            pokemons_map.iloc[i,1]))
+                
+            if types_pokemon.iloc[j,2] == "Electric" :
+                
+                Pokemon_map.append(p.Electric(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                              types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                              types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                              types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                              pokemons_map.iloc[i,1]))
+                
+            if types_pokemon.iloc[j,2] == "Poison" :
+                
+                Pokemon_map.append(p.Poison(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                            types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                            types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                            types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                            pokemons_map.iloc[i,1]))                
+                
+            if types_pokemon.iloc[j,2] == "Ground" :
+                
+                Pokemon_map.append(p.Ground(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                            types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                            types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                            types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                            pokemons_map.iloc[i,1]))                
+                
+            if types_pokemon.iloc[j,2] == "Fairy" :
+                
+                Pokemon_map.append(p.Fairy(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                           types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                           types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                           types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                           pokemons_map.iloc[i,1]))                
+                
+            if types_pokemon.iloc[j,2] == "Fighting" :
+                
+                Pokemon_map.append(p.Fighting(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                              types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                              types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                              types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                              pokemons_map.iloc[i,1]))                
+                
+            if types_pokemon.iloc[j,2] == "Psychic" :
+                
+                Pokemon_map.append(p.Psychic(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                             types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                             types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                             types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                             pokemons_map.iloc[i,1]))                
+                
+            if types_pokemon.iloc[j,2] == "Ice" :
+                
+                Pokemon_map.append(p.Ice(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                         types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                         types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                         types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                         pokemons_map.iloc[i,1]))                
+                
+            if types_pokemon.iloc[j,2] == "Flying" :
+                
+                Pokemon_map.append(p.Flying(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                            types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                            types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                            types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                            pokemons_map.iloc[i,1]))                
+                
+            if types_pokemon.iloc[j,2] == "Dragon" :
+                
+                Pokemon_map.append(p.Dragon(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                            types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                            types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                            types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                            pokemons_map.iloc[i,1]))                
+           
+            if types_pokemon.iloc[j,2] == "Ghost" :
+                
+                Pokemon_map.append(p.Ghost(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                           types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                           types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                           types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                           pokemons_map.iloc[i,1]))            
+            
+            if types_pokemon.iloc[j,2] == "Rock" :
+                
+                Pokemon_map.append(p.Rock(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                          types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                          types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                          types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                          pokemons_map.iloc[i,1]))            
+            
+            if types_pokemon.iloc[j,2] == "Grass" :
+                
+                Pokemon_map.append(p.Grass(types_pokemon.iloc[j,1],types_pokemon.iloc[j,6], 
+                                           types_pokemon.iloc[j,7],types_pokemon.iloc[j,8], 
+                                           types_pokemon.iloc[j,9], types_pokemon.iloc[j,10],
+                                           types_pokemon.iloc[j,11],types_pokemon.iloc[j,12],  
+                                           pokemons_map.iloc[i,1]))            
+                
+                
+                
+                
