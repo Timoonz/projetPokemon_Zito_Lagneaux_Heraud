be174a7c90fe6bb518cba7720d4e3f693968d24d
Timothée HERAUD <timotheeeheraud@gmail.com>
2024-05-17 19:25:36 +0200

    Amélioration de la lisibilité du fichier

--- scripts_python/main.py
+++ scripts_python/main.py
@@ -6,12 +6,11 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow
-from GUI_Combat_code import *
+import GUI_Combat_code as combat
+import Extraction_des_donnes as data
 from  PyQt5.QtTest import QTest
 import Inventory as inv
 import POKEMON as pk
-
-from PyQt5.QtMultimedia import QSound
 
 
 class Game(QMainWindow) :
@@ -23,25 +22,25 @@ class Game(QMainWindow) :
     def keyReleaseEvent(self, event) : 
         if event.key() == Qt.Key_Up and not event.isAutoRepeat():
             self.map.move("up")
-            if joueur.detecter(Pokemon_sauvage)[0]:
+            if pk.joueur.detecter(data.Pokemon_sauvage)[0]:
                 self.open_fight()
                 
                 
         if event.key() == Qt.Key_Down and not event.isAutoRepeat(): 
             self.map.move("down")
-            if joueur.detecter(Pokemon_sauvage)[0]:
+            if pk.joueur.detecter(data.Pokemon_sauvage)[0]:
                 self.open_fight()
                 
                 
         if event.key() == Qt.Key_Left and not event.isAutoRepeat(): 
             self.map.move("left")
-            if joueur.detecter(Pokemon_sauvage)[0]:
+            if pk.joueur.detecter(data.Pokemon_sauvage)[0]:
                 self.open_fight()
                 
                 
         if event.key() == Qt.Key_Right and not event.isAutoRepeat(): 
             self.map.move("right")
-            if pk.joueur.detecter(Pokemon_sauvage)[0]:
+            if pk.joueur.detecter(data.Pokemon_sauvage)[0]:
                 self.open_fight()
         
         # Pour afficher l'inventaire
@@ -49,7 +48,7 @@ class Game(QMainWindow) :
             self.open_inventory()
             
         if event.key() == Qt.Key_C :
-            joueur.chanter()
+            pk.joueur.chanter()
             
         
         ##Processus qui nous a permis de faire la carte des collisions 
@@ -95,16 +94,16 @@ class Game(QMainWindow) :
     def open_fight(self):
         
         
-        joueur.pokemon_adverse = joueur.detecter(Pokemon_sauvage)[1]
-        self.fight = Combat()
+        pk.joueur.pokemon_adverse = pk.joueur.detecter(data.Pokemon_sauvage)[1]
+        self.fight = combat.Combat()
         self.fight.barre_hp_adverse.setValue(100)
         self.fight.barre_hp_choisi.setValue(100)
         self.fight.reset_hp()
         self.fight.update()
-        self.fight.textBrowser.setText("Un " + joueur.pokemon_adverse.name + " est apparu !")        
+        self.fight.textBrowser.setText("Un " + pk.joueur.pokemon_adverse.name + " est apparu !")        
         self.fight.show()
         QTest.qWait(2000)
-        self.fight.textBrowser.setText("C'est un pokemon de type " + joueur.pokemon_adverse.type_pokemon() + ".")
+        self.fight.textBrowser.setText("C'est un pokemon de type " + pk.joueur.pokemon_adverse.type_pokemon() + ".")
         
         
     def open_inventory(self):
